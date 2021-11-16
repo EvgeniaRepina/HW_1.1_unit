0eae4f72c86e1cbe03b179da6c724431e01bc8de
EvgeniaRepina <88374316+EvgeniaRepina@users.noreply.github.com>
2021-11-16 12:33:38 +0100

    corrections

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -40,7 +40,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = CashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
